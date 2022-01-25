f66da5fc356d8fb84f9eb9fb998729509883cdb7
Gugu7264 <github@gurvan.fr>
2022-01-25 13:28:14 +0100

    chore: change test

--- tests/embed.py
+++ tests/embed.py
@@ -8,6 +8,11 @@ embed._provider = EmbedProvider(name="Discord", url="https://discord.com/")
 data = embed.to_dict()
 print(data)
 embed2 = Embed.from_dict(data)
+print(embed2, dir(embed2))
+for a in dir(embed2):
+    if not a.startswith("__"):
+        print(a, getattr(embed2, a))
+
 data2 = embed2.to_dict()
 
 print(data2)
