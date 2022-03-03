5607b0a87bec61ca8e6bc0ec4dfb4bff7de5e6ea
skelmis <skelmis.craft@gmail.com>
2022-03-04 03:11:48 +1300

    Resolve conflicts

--- nextcord/core/http.py
+++ nextcord/core/http.py
@@ -25,7 +25,7 @@ from asyncio import Future, get_event_loop
 from collections import defaultdict
 from logging import getLogger
 from time import time
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Type
 
 from aiohttp import ClientSession
 
@@ -47,6 +47,19 @@ logger = getLogger(__name__)
 
 
 class Route(RouteProtocol):
+    """Metadata about a Discord API route
+    Parameters
+    ----------
+    method: :class:`str`
+        The HTTP method for this route
+    path: :class:`str`
+        The API path
+    use_webhook_global: :class:`bool`
+        If this route uses the webhook global LINK MISSING
+    parameters:
+        Parameters to format path with. You can include guild_id, channel_id, webhook_id or webhook_token to specify ratelimit parameters.
+    """
+
     def __init__(
         self,
         method: Literal[
@@ -66,10 +79,14 @@ class Route(RouteProtocol):
         **parameters: Any,
     ):
         self.method = method
+        """The HTTP method for this route"""
         self.unformatted_path = path
+        """The unformatted path"""
         self.path = path.format(**parameters)
+        """The route to be requested from discord"""
 
         self.use_webhook_global = use_webhook_global
+        """If this route uses the webhook global LINK MISSING"""
 
         self.guild_id: Optional[int] = parameters.get("guild_id")
         self.channel_id: Optional[int] = parameters.get("channel_id")
@@ -77,33 +94,45 @@ class Route(RouteProtocol):
         self.webhook_token: Optional[str] = parameters.get("webhook_token")
 
     @property
-    def bucket(self) -> str:
+    def bucket(self) -> str:  # type: ignore
+        """The ratelimit bucket this is under"""
         return f"{self.method}:{self.unformatted_path}:{self.guild_id}:{self.channel_id}:{self.webhook_id}:{self.webhook_token}"
 
 
 class Bucket(BucketProtocol):
+    """A simple and fast ratelimiting implementation for HTTP
+    .. warning::
+        This is not multiprocess safe.
+    .. note::
+        This is a async context manager.
+    """
+
     def __init__(self, route: Route):
         self._remaining: Optional[int] = None
         self.limit: Optional[int] = None
+        """How many requests fit in a bucket"""
         self.reset_at: Optional[float] = None
+        """When the Bucket fills up again. (UTC time)"""
         self._route: Route = route
-        self._pending: list[Future] = []
+        self._pending: list[Future[None]] = []
         self._reserved: int = 0
         self._loop = get_event_loop()
 
-    @property
-    def remaining(self) -> Optional[int]:
+    @property  # type: ignore
+    def remaining(self) -> Optional[int]:  # type: ignore
+        """How many requests are remaining."""
         return self._remaining
 
     @remaining.setter
-    def remaining(self, new_value: int):
+    def remaining(self, new_value: int) -> None:
         self._remaining = new_value
         if new_value == 0:
             self._pending_reset = True
             sleep_time = self.reset_at - time()
             self._loop.call_later(sleep_time, self._reset)
 
-    def _reset(self):
+    def _reset(self) -> None:
+        """Reset the bucket usage to the top and then start attempting to release the pending requests"""
         self._remaining = self.limit
 
         for _ in range(self._calculated_remaining):
@@ -115,17 +144,22 @@ class Bucket(BucketProtocol):
 
     @property
     def _calculated_remaining(self) -> int:
+        # TODO: Replace this with the getter of remaining
         if self.remaining is None:
             return 1  # We have no data, let's just assume we have one request so we can fetch the info.
         return self.remaining - self._reserved
 
-    async def __aenter__(self) -> "BucketProtocol":
+    async def __aenter__(self) -> "Bucket":
+        """Reserve a spot in the bucket for the request.
+        If all are taken, it will add it to a queue.
+        """
+        # TODO: This should return same type as itself. Not sure what's wrong when I try
         if self.remaining is None:
             self._reserved += 1
             return self  # We have no ratelimiting info, let's just try
         if self._calculated_remaining <= 0:
             # Ratelimit pending, let's wait
-            future = Future()
+            future: Future[None] = Future()
             self._pending.append(future)
             logger.debug(
                 "Waiting for %s to clear up. %s pending", str(self), len(self._pending)
@@ -134,13 +168,25 @@ class Bucket(BucketProtocol):
         self._reserved += 1
         return self
 
-    async def __aexit__(self, exc_type, exc, tb):
+    async def __aexit__(self, *_: Any) -> None:
+        """
+        Request finished
+        """
         self._reserved -= 1
         if self.remaining is not None:
             self.remaining -= 1
 
 
 class HTTPClient(HTTPClientProtocol):
+    """A http client to interact with the Discord REST API.
+    Parameters
+    ----------
+    state: :class:`State`
+        The current state of the bot
+    max_retries: :class:`int`
+        How many times we will attempt to retry after a unexpected failure (server error or ratelimit issue)
+    """
+
     def __init__(
         self,
         state: State,
@@ -159,7 +205,9 @@ class HTTPClient(HTTPClientProtocol):
         )
         self._session = ClientSession(json_serialize=json.dumps)
         self._buckets: dict[str, BucketProtocol] = {}
-        self._http_errors = defaultdict((lambda: HTTPException), {})
+        self._http_errors: defaultdict[int, Type[HTTPException]] = defaultdict(
+            (lambda: HTTPException), {}
+        )
 
         self._headers = {
             "User-Agent": "DiscordBot (https://github.com/nextcord/nextcord, {})".format(
@@ -171,11 +219,23 @@ class HTTPClient(HTTPClientProtocol):
 
     async def request(
         self,
-        route: Route,
+        route: RouteProtocol,
         *,
-        headers: dict[str, Any] = None,
-        **kwargs,
+        headers: Optional[dict[str, str]] = None,
+        **kwargs: Any,
     ) -> ClientResponse:
+        """Send a request to discord.
+        This automatically handles ratelimits.
+        .. versionadded:: 3.0
+        Parameters
+        ----------
+        route: :class:`RouteProtocol`
+            Metadata about the route you are executing
+        headers: :class:`Optional[dict[str, str]]`
+            Request headers. This will add a bot token if availible
+        kwargs:
+            Keyword only arguments passed to `ClientSession.request <https://docs.aiohttp.org/en/stable/client_reference.html#aiohttp.ClientSession.trace_config>`_
+        """
         global_lock = (
             self._webhook_global_lock if route.use_webhook_global else self._global_lock
         )
@@ -224,19 +284,18 @@ class HTTPClient(HTTPClientProtocol):
                 return r
 
         raise DiscordException(
-            "Ratelimiting failed 5 times. This should only happen if you are running multiple bots with the same IP."
+            f"Ratelimiting failed {self.max_retries} times. This should only happen if you are running multiple bots with the same IP."
         )
 
-    async def ws_connect(self, url) -> ClientWebSocketResponse:
+    async def ws_connect(self, url: str) -> ClientWebSocketResponse:
         return await self._session.ws_connect(
             url, max_msg_size=0, autoclose=False, headers=self._headers
         )
 
-    async def close(self):
+    async def close(self) -> None:
         await self._session.close()
 
     # Wrappers around the http methods
-    async def get_gateway_bot(self):
+    async def get_gateway_bot(self) -> ClientResponse:
         route = Route("GET", "/gateway/bot")
-
         return await self.request(route)
